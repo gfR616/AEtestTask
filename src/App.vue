08d6f7af328c370f803e43a90f9a2a7b61bb0d12
Roman <616gfr@gmail.com>
2025-12-06 16:28:49 +0500

    шрифты+глобал css

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,19 @@ import navPanel from './app/shared/nav-panel.vue'
   <router-view />
 </template>
 
-<style scoped></style>
+<style>
+:root {
+  --nav-bar-color: #165996;
+  --background-color: #e2e1e1;
+  --buttons-color-primary: #d9d9d9;
+  --buttons-color-secondary: #ffffff;
+}
+body {
+  margin: 0;
+  font-family: 'Inter', sans-serif;
+  font-optical-sizing: auto;
+  font-style: 12px;
+  font-weight: 400;
+  background: var(--background-color);
+}
+</style>
