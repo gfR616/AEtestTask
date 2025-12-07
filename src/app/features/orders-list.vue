7f5ac11aa4b4830ece6259cc94723159de322409
Roman <616gfr@gmail.com>
2025-12-07 15:16:15 +0500

    запрашиваем и выводим в список

--- src/app/features/orders-list.vue
+++ src/app/features/orders-list.vue
@@ -12,10 +12,29 @@
         </tr>
       </thead>
 
-      <tbody id="orders-body"></tbody>
+      <tbody id="orders-body">
+        <tr v-for="order in ordersStore.orders">
+          <td>{{ order.id }}</td>
+          <td>{{ order.name }}</td>
+          <td>{{ order.address }}</td>
+          <td>{{ order.date }}</td>
+          <td>{{ order.status }}</td>
+          <td>{{ order.comment }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
+
+<script setup>
+import { useOrdersStore } from '@/stores/ordersStore'
+import { onMounted } from 'vue'
+
+const ordersStore = useOrdersStore()
+onMounted(() => {
+  ordersStore.fetchOrders()
+})
+</script>
 
 <style scoped>
 .table-container {
