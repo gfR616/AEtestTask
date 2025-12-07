e9af319da9731adb167986ab588583dedb804e6a
Roman <616gfr@gmail.com>
2025-12-07 20:06:57 +0500

    увы, пост не получился

--- src/app/core/services/authService.ts
+++ src/app/core/services/authService.ts
@@ -1,6 +1,9 @@
 import type { User } from '../types/types'
 import apiClient from '@/api/api'
 
+//Да, мне известно, что в ТЗ указан POST запрос,
+//но мы работаем с JSON-сервером,
+//POST он использует для добавления данных. Поэтому GET
 export const authService = {
   async login(username: string, pass: string) {
     const res = await apiClient.get<User[]>('/users', {
