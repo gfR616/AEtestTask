13bfa97d6b4e41ef75960c759fe995e4f6aff9e9
Roman <616gfr@gmail.com>
2025-12-07 15:58:11 +0500

    выводим таблицу

--- src/app/features/orders-list.vue
+++ src/app/features/orders-list.vue
@@ -1,37 +1,42 @@
 <template>
-  <div class="table-container">
-    <table class="orders-table">
-      <thead>
-        <tr>
-          <th>№</th>
-          <th>Имя клиента</th>
-          <th>Адрес</th>
-          <th>Дата заказа</th>
-          <th>Статус</th>
-          <th>Комментарий</th>
-        </tr>
-      </thead>
+  <div class="container">
+    <div class="table-container">
+      <table class="orders-table">
+        <thead>
+          <tr>
+            <th>№</th>
+            <th>Имя клиента</th>
+            <th>Адрес</th>
+            <th>Дата заказа</th>
+            <th>Статус</th>
+            <th>Комментарий</th>
+          </tr>
+        </thead>
 
-      <tbody id="orders-body">
-        <tr v-for="order in ordersStore.orders">
-          <td>{{ order.id }}</td>
-          <td>{{ order.name }}</td>
-          <td>{{ order.address }}</td>
-          <td>{{ order.date }}</td>
-          <td>{{ order.status }}</td>
-          <td>{{ order.comment }}</td>
-        </tr>
-      </tbody>
-    </table>
+        <tbody id="orders-body">
+          <tr v-for="order in ordersStore.orders">
+            <td>{{ order.id }}</td>
+            <td>{{ order.name }}</td>
+            <td>{{ order.address }}</td>
+            <td>{{ order.date }}</td>
+            <td>{{ order.status }}</td>
+            <td>{{ order.comment }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { useAuthStore } from '@/stores/authStore'
 import { useOrdersStore } from '@/stores/ordersStore'
 import { onMounted } from 'vue'
 
 const ordersStore = useOrdersStore()
+const authStore = useAuthStore()
 onMounted(() => {
+  // authStore.getUser()
   ordersStore.fetchOrders()
 })
 </script>
