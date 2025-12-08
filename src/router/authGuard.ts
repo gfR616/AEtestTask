6571b4ecd5f32f05df884d16f401585112e7802a
Roman <616gfr@gmail.com>
2025-12-08 18:11:20 +0500

    инициализируем пинию в гварде

--- src/router/authGuard.ts
+++ src/router/authGuard.ts
@@ -1,9 +1,10 @@
+import type { Pinia } from 'pinia'
 import type { Router } from 'vue-router'
 import { useAuthStore } from '../stores/authStore'
 
-export function setupRouterGuards(router: Router) {
+export function setupRouterGuards(router: Router, pinia: Pinia) {
   router.beforeEach((to, from, next) => {
-    const authStore = useAuthStore()
+    const authStore = useAuthStore(pinia)
     const isAuthenticated = !!authStore.user
 
     if (to.path === '/auth' && isAuthenticated) {
